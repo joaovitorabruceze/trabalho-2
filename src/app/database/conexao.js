7a326742975bd6ca6617d87c242573b8c07d0de0
joaovitorabruceze <joaovitorabruceze@hotmail.com>
2024-02-18 23:15:20 -0300

    ajustando conexao banco

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -2,12 +2,12 @@ import mysql from 'mysql';
 import fs from 'fs';
 
 const conexao = mysql.createConnection({
-    server: 'trabalho2.mysql.database.azure.com',//localhost
-    port: '3306',//3306
+    host: 'trabalho2.mysql.database.azure.com',//localhost
     user: 'administrador',//root
     password: 'Root0000',//root
-    database: 'trabalho2'//trabalho2
-    , ssl: { ca: fs.readFileSync('src/app/database/DigiCertGlobalRootCA.crt.pem') }
+    database: 'trabalho2',//trabalho2
+    port: 3306,//3306
+    ssl: { ca: fs.readFileSync('src/app/database/DigiCertGlobalRootCA.crt.pem') }
 });
 
 conexao.connect();
